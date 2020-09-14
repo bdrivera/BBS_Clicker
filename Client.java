78b71d4260e8304c519f57b6d3093ca81edef31d
Bret Rivera <bdrrivera@gmail.com>
2020-09-14 15:48:17 -0700

    Renamed ScreenTasker to Client -Rename falls in line with plan to extend class later -Changed extension of Thread to implementation of Runnable

--- Client.java
+++ Client.java
@@ -9,19 +9,29 @@ import java.awt.event.KeyEvent;
  * Uses ImageScanner class to check and handle if certain images are on screen
  */
 
-public class ScreenTasker extends Thread {
+public class Client implements Runnable {
 
     private Robot rob = new Robot();
+
+    private boolean running = false;
 
     /**
      * the images of the buttons to be checked for
      */
     BufferedImage[] image = new image[5];
 
-    public void run() {
 
+    public void run() {
+        while(running) {
+
+        }
     }
 
+    /**
+     * Uses class Robot instance to click on screen using Operating System mouse.
+     * @param x horizontal coordinate of the screen
+     * @param y vertical coordinate of the screen
+     */
     private void leftClickOnLocation(int x, int y) {
         rob.mouseMove(x, y);
         rob.mousePress(InputEvent.BUTTON1_MASK);
