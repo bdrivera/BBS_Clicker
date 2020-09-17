2b47ef1a7929e7139edbffabb34c3b5c8991cca5
Bret Rivera <bdrrivera@gmail.com>
2020-09-16 19:31:55 -0700

    Added logic to Host, modified way image comparison is done, added accessor methods, adding report back functions -Host will handle invites, raiding, and basic menu navigation -Added simpler method in Client to compare images -Added additional flags to enhance communication between clients -More accessor methods to access flags -Added logic for "ticks" that are a time measurement for iterations of Client thread loops

--- src/Client.java
+++ src/Client.java
@@ -24,8 +24,14 @@ public abstract class Client implements Runnable {
     private static Hashtable<String, BufferedImage>
             hmRefImages = new Hashtable<String, BufferedImage>();
 
-    public boolean running = false; // Flag for whether or not to keep looping the flag
-    public static boolean raidFlag = false; // Flag for if a raid invite has been sent
+    private static int muleCount;
+    private static int muleReport = 0;
+
+    private static boolean running = false; // Flag for whether or not to keep looping the flag
+    private static boolean raidFlag = false; // Flag for if a raid is in progress
+    private static boolean inviteFlag = false; // Flag for if a raid invite has been sent
+    private boolean isInError = false; // Flag for if this client is experiencing an error
+
     private Rectangle clientArea; // The area of responsibility for this client.
 
     // The offset values used to find actual coordinates
@@ -33,6 +39,9 @@ public abstract class Client implements Runnable {
     private int yOffset;
 
     BufferedImage clientAreaImage; // The images of the buttons to be checked for
+
+    public static final int TICK_TIME = 500; // Time in miliseconds for each tick iteration
+    public static final int TICK_MULTI = 1000 / TICK_TIME; // Used to scale ticks to seconds
 
     /**
      * Client is a class meant to be parent to Host and Mule classes
@@ -42,39 +51,15 @@ public abstract class Client implements Runnable {
      * @param y2 bottom right y coordinate of client windows
      * @throws AWTException, IOException
      */
-    public Client(int x1, int y1, int x2, int y2) throws AWTException, IOException {
+    public Client(int x1, int y1, int x2, int y2, int mules) throws AWTException, IOException {
         int width = x2 - x1;
         int height = y2 - y1;
         clientArea = new Rectangle(x1, y1, width, height);
         xOffset = x1;
         yOffset = y1;
+        muleCount = 6;
         loadResourceHash();
-    }
-
-    /**
-     * Used to get a screenshot of the area of responsibility of the client
-     * @return screen shot of the area of responsibility
-     */
-    public BufferedImage getClientAreaImage() {
-        return rob.createScreenCapture(clientArea);
-    }
-
-    /**
-     * Used to get the x coordinate relative to entire screen
-     * @param x x coordinate of area of responsibility
-     * @return screen x coordinate
-     */
-    public int getRelativeX(int x) {
-        return x + xOffset;
-    }
-
-    /**
-     * Used to get the y coordinate relative to entire screen
-     * @param y y coordinate of area of responsibility
-     * @return screen y coordinate
-     */
-    public int getRelativeY(int y) {
-        return y + yOffset;
+        startRunning();
     }
 
     /**
@@ -82,11 +67,20 @@ public abstract class Client implements Runnable {
      * @param x horizontal coordinate of the screen
      * @param y vertical coordinate of the screen
      */
-    private synchronized void leftClickOnLocation(int x, int y) {
+    public synchronized void leftClickOnLocation(int x, int y) {
         rob.mouseMove(x, y);
         rob.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         rob.delay(200);
         rob.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    /**
+     * Uses ImageScanner to check if the reference image is in the Area of Responsibility
+     * @param key key for desired reference image
+     * @return if reference is on screen or not
+     */
+    public boolean isInAOR(String key) {
+        return scan.isOnScreen(getClientAreaImage(), getReferenceImage(key));
     }
 
     /**
@@ -125,4 +119,145 @@ public abstract class Client implements Runnable {
     public BufferedImage getReferenceImage(String key) {
         return hmRefImages.get(key);
     }
+
+    /**
+     * Used to get a screenshot of the area of responsibility of the client
+     * @return screen shot of the area of responsibility
+     */
+    public BufferedImage getClientAreaImage() {
+        return rob.createScreenCapture(clientArea);
+    }
+
+    /**
+     * Used to get the x coordinate relative to entire screen
+     * @param x x coordinate of relative to entire screen
+     * @return screen x coordinate
+     */
+    public int getRelativeX(int x) {
+        return x + xOffset;
+    }
+
+    /**
+     * Used to get the y coordinate relative to entire screen
+     * @param y y coordinate of relative to entire screen
+     * @return screen y coordinate
+     */
+    public int getRelativeY(int y) {
+        return y + yOffset;
+    }
+
+    /**
+     * Used to get the x coordinate area of responsibility
+     * @return screen x coordinate
+     */
+    public int getAORX() {
+        return scan.getFoundX() + xOffset;
+    }
+
+    /**
+     * Used to get the y coordinate area of responsibility
+     * @return screen y coordinate
+     */
+    public int getAORY() {
+        return scan.getFoundY() + yOffset;
+    }
+
+    /**
+     * Returns the current condition of the raid flag
+     * @return raid flag condition
+     */
+    public boolean isRaiding() {
+        return raidFlag;
+    }
+
+    /**
+     * Switches the condition of the raid flag to opposite value
+     */
+    public void switchRaidFlag() {
+        raidFlag = raidFlag ? false : true;
+    }
+
+    /**
+     * Returns the current condition of the invite flag
+     * @return invite flag condition
+     */
+    public boolean isInvited() {
+        return raidFlag;
+    }
+
+    /**
+     * Switches the condition of the invite flag to opposite value
+     */
+    public void switchInviteFlag() {
+        inviteFlag = inviteFlag ? false : true;
+    }
+
+    /**
+     * Returns the current condition of the error flag
+     * @return error flag condition
+     */
+    public boolean isInError() {
+        return isInError();
+    }
+
+    /**
+     * Switches the condition of the error flag to opposite value
+     */
+    public void switchInError() {
+        isInError = isInError ? false : true;
+    }
+
+    /**
+     * Returns the current condition of running
+     * @return running flag condition
+     */
+    public boolean isRunning() {
+        return running;
+    }
+
+    /**
+     * Changes running flag to true
+     */
+    public void startRunning() {
+        running = true;
+    }
+
+    /**
+     * Changes running flag to false
+     */
+    public void stopRunning() {
+        running = false;
+    }
+
+    /**
+     * Accessor method for muleCount
+     * @return amount of mules running
+     */
+    public int getMuleCount() {
+        return muleCount;
+    }
+
+    /**
+     * Accessor method for muleReport
+     * @return number of mules who have reported
+     */
+    public int getMuleReport() {
+        return muleReport;
+    }
+
+    /**
+     * Resets the mule report count to 0
+     */
+    public void resetMuleReport() {
+        muleReport = 0;
+    }
+
+    /**
+     * Adds 1 to the mule report count
+     */
+    public synchronized void reportMule() {
+        muleReport++;
+    }
+
+
 }
--- src/Host.java
+++ src/Host.java
@@ -10,15 +10,55 @@ import java.io.IOException;
  */
 public class Host extends Client implements Runnable {
 
-    public Host(int x1, int y1, int x2, int y2) throws AWTException, IOException {
-        super(x1, y1, x2, y2);
+    public Host(int x1, int y1, int x2, int y2, int mules) throws AWTException, IOException {
+        super(x1, y1, x2, y2, mules);
     }
 
     @Override
     public void run() {
-        running = true;
-        while(running) {
-            BufferedImage activeScreen = getClientAreaImage();
+        while(isRunning()) { //getClientAreaImage();
+            try {
+                Thread.sleep(TICK_TIME); // sleeps a tick
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if(isInError()) {
+
+                continue;
+            } else if(isInvited()) { // If you have sent the invite...
+                if(getMuleReport() == getMuleCount()) {
+                    if(isInAOR("erStartQuest")) {
+                        leftClickOnLocation(getAORX(), getAORY());
+                    } else if(isInAOR("startRaid")) {
+                        leftClickOnLocation(getAORX(), getAORY());
+                        switchInviteFlag();
+                        switchRaidFlag();
+                    }
+                }
+                continue;
+            } else if(isRaiding()) { // If you are currently in a raid...
+
+                if(isInAOR("autoOn")) {
+                    leftClickOnLocation(getAORX(), getAORY());
+                } else if(isInAOR("retry")) {
+                    switchRaidFlag();
+                    leftClickOnLocation(getAORX(), getAORY());
+                }
+                continue;
+
+            } else { // If you are not special...
+
+                if(isInAOR("createRoom")) {
+                    leftClickOnLocation(getAORX(), getAORY());
+                } else if(isInAOR("inviteIcon")) {
+                    leftClickOnLocation(getAORX(), getAORY());
+                } else if(isInAOR("ok")) {
+                    leftClickOnLocation(getAORX(), getAORY());
+                    switchInviteFlag();
+                }
+
+            }
         }
     }
 }
